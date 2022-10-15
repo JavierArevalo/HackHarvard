64152dfd15e8c1923b149ce2503503f4be23c765
Rishabh0896 <36626866+Rishabh0896@users.noreply.github.com>
2022-10-15 17:40:38 -0400

    Updated API endpoints

--- app.py
+++ app.py
@@ -13,24 +13,24 @@ cred = credentials.Certificate('HackHarvard.json')
 default_app = initialize_app(cred, {
     'databaseURL': 'https://hackharvard-ba0b8.firebaseio.com/'
 })
-company_ref = db.reference('/Companies')
+company_ref = db.reference('/companies')
+investor_ref = db.reference('/investors')
 
+# @app.route('/add', methods=['POST'])
+# def create():
+#     """
+#         create() : Add document to Firestore collection with request body.
+#         Ensure you pass a custom ID as part of json body in post request,
+#         e.g. json={'id': '1', 'title': 'Write a blog post'}
+#     """
+#     try:
+#         id = request.json['id']
+#         company_ref.document(id).set(request.json)
+#         return jsonify({"success": True}), 200
+#     except Exception as e:
+#         return f"An Error Occurred: {e}"
 
-@app.route('/add', methods=['POST'])
-def create():
-    """
-        create() : Add document to Firestore collection with request body.
-        Ensure you pass a custom ID as part of json body in post request,
-        e.g. json={'id': '1', 'title': 'Write a blog post'}
-    """
-    try:
-        id = request.json['id']
-        company_ref.document(id).set(request.json)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occurred: {e}"
-
-
+# Currently, in use
 @app.route('/list', methods=['GET'])
 def read():
     """
@@ -50,50 +50,21 @@ def read():
     except Exception as e:
         return f"An Error Occurred: {e}"
 
-@app.route('/listAll', methods=['GET'])
-def read():
-    """
-        read() : Fetches documents from Firestore collection as JSON.
-        todo : Return document that matches query ID.
-        all_todos : Return all documents.
-    """
-    try:
-        # Check if companyName was passed to URL query
-        com_nam = request.args.get()
-        if com_nam:
-            company = company_ref.get(com_nam)
-            return jsonify(company), 200
-        else:
-            all_companies = [doc.to_dict() for doc in company_ref.stream()]
-            return jsonify(all_companies), 200
-    except Exception as e:
-        return f"An Error Occurred: {e}"
 
-
-@app.route('/update', methods=['POST', 'PUT'])
-def update():
+@app.route('/invest', methods=['POST', 'PUT'])
+def invest():
     """
-        update() : Update document in Firestore collection with request body.
+        invest() : Invest in the company.
+        Does the following things:
+        1: Reduce the number of NFTs available from companies
+        2: Add the company to the investor's portfolio
+        3: Returns success if all the above operations are successful.
         Ensure you pass a custom ID as part of json body in post request,
         e.g. json={'id': '1', 'title': 'Write a blog post today'}
     """
     try:
         id = request.json['id']
         company_ref.document(id).update(request.json)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occurred: {e}"
-
-
-@app.route('/delete', methods=['GET', 'DELETE'])
-def delete():
-    """
-        delete() : Delete a document from Firestore collection.
-    """
-    try:
-        # Check for ID in URL query
-        todo_id = request.args.get('id')
-        company_ref.document(todo_id).delete()
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occurred: {e}"
