20ae7e2aa07ad08d8e83b661983c85ebfb7d530e
Rishabh0896 <36626866+Rishabh0896@users.noreply.github.com>
2022-10-14 23:06:49 -0400

    Investor Object Base Commit

---
+++ Investor.py
@@ -0,0 +1,28 @@
+## Mimics a Real World Investor
+
+class Investor:
+
+    ## Investor Level --> can deepend on the amountInvestment,income and other factors
+    ## TODO: Feature
+    def __init__(self, name, email, amtInvestment, walletHash, companyList, investorLevel):
+        self.__init__(self, name, email, amtInvestment ,walletHash, companyList)
+        self.investorLevel = investorLevel
+
+    ## Default constructor to construct an Investor
+    def __init__(self, name, email, amtInvestment, walletHash, companyList):
+        self.name = name
+        self.email = email
+        self.amtInvestment = amtInvestment
+        self.investorHash = hash(name,email)
+        self.walletHash = walletHash
+        self.companyList = companyList
+        self.investorLevel = investorLevel
+
+
+    ## Differentiating investors basis unique emailId
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.email == other.email
+
+    def __hash__(self):
+        return hash(self.email)
